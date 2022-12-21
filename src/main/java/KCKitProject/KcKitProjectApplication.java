5d5302d0c970aba4640990e6cfc66f749699a679
Matthew Temple <mattdtemple@gmail.com>
2022-12-20 21:14:47 -0600

    Intial Commit. Database link

---
+++ src/main/java/KCKitProject/KcKitProjectApplication.java
@@ -0,0 +1,13 @@
+package KCKitProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcKitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcKitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KCKitProject/KcKitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package KCKitProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcKitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
