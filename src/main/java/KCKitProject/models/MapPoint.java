b7ce3e13923f0c8ff9ce952a4ac42ae0c31b03c8
Matthew Temple <mattdtemple@gmail.com>
2023-02-11 14:02:00 -0600

    MapPoint model

---
+++ src/main/java/KCKitProject/models/MapPoint.java
@@ -0,0 +1,19 @@
+package KCKitProject.models;
+
+class MapPoint {
+    private double lat;
+    private double lng;
+
+    public MapPoint(double lat, double lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+}
